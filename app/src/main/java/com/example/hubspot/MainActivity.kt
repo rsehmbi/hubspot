5007f8f6b56a82a079fe018f29ffa53bd714c5d4
rsehmbi <rsehmbi@sfu.ca>
2022-07-09 21:06:04 -0700

    feat: adds bottom navigation and fragments

--- app/src/main/java/com/example/hubspot/MainActivity.kt
+++ app/src/main/java/com/example/hubspot/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.hubspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hubspot.ratings.RatingsFragment
+import com.example.hubspot.schedule.ScheduleFragment
+import com.example.hubspot.security.SecurityFragment
+import com.example.hubspot.studybuddy.StudyBuddyFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ratingsFragment = RatingsFragment()
+        val scheduleFragment = ScheduleFragment()
+        val securityFragment = SecurityFragment()
+        val studybuddyFragment = StudyBuddyFragment()
+
+        setCurrentFragment(scheduleFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.schedule_id->setCurrentFragment(scheduleFragment)
+                R.id.ratings_id->setCurrentFragment(ratingsFragment)
+                R.id.security_id->setCurrentFragment(securityFragment)
+                R.id.studybuddy_id->setCurrentFragment(studybuddyFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
