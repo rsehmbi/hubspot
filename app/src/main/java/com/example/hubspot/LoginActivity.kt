4cbac409caf43ef5914315d5e7e117e553632758
Noel Riopel <nriopel@sfu.ca>
2022-07-13 19:26:32 -0700

    Remove the sign up activity

--- app/src/main/java/com/example/hubspot/LoginActivity.kt
+++ app/src/main/java/com/example/hubspot/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.hubspot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun onLogInButtonClick(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun onSignUpButtonClick(view: View) {}
 }
