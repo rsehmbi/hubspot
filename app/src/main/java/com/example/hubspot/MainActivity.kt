77b3278a5221269e05212eeb9190082d210bbda4
Noel Riopel <nriopel@sfu.ca>
2022-07-13 19:52:39 -0700

    Create mock auth service methods and navigation

--- app/src/main/java/com/example/hubspot/MainActivity.kt
+++ app/src/main/java/com/example/hubspot/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.hubspot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.hubspot.ratings.RatingsFragment
 import com.example.hubspot.schedule.ScheduleFragment
 import com.example.hubspot.security.SecurityFragment
+import com.example.hubspot.services.Auth
 import com.example.hubspot.studybuddy.StudyBuddyFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val currentUser = Auth.getCurrentUser()
+        if (currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         setContentView(R.layout.activity_main)
 
         val ratingsFragment = RatingsFragment()
---
+++ app/src/main/java/com/example/hubspot/services/Auth.kt
@@ -0,0 +1,45 @@
+package com.example.hubspot.services
+
+object Auth {
+    // Will be removed, for mock navigation flow purposes.
+    var initialLogin: Boolean = true
+
+    /** Returns a User object for the currently logged in user.
+     *  Returns null if there is no logged in user. */
+    fun getCurrentUser(): User? {
+        if (initialLogin) {
+            initialLogin = false
+            return null
+        } else {
+            return User(0, "testing@email.com")
+        }
+    }
+
+    /** Sends an account activation email to the user. Once the user clicks
+     *  on the account activation link in the email, they can log in.
+     *  Returns a status code that describes the result of the operation. */
+    fun sendAccountActivationEmail(
+        email: String,
+        password: String
+    ): SendAccountActivationEmailResult {
+        return SendAccountActivationEmailResult.SUCCESS
+    }
+
+    enum class SendAccountActivationEmailResult {
+        SUCCESS,
+        FAILURE
+    }
+
+    /** Attempts to sign in the user with a given email and password.
+     *  Returns an error code that describes the result of the operation. */
+    fun signInUser(email: String, password: String): SignInUserResult {
+        return SignInUserResult.SUCCESS
+    }
+
+    enum class SignInUserResult {
+        SUCCESS,
+        FAILURE
+    }
+}
+
+class User(val id: Int, val email: String) {}
