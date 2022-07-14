1ec8ca0fcb00c62c7f73ffaca12c7a0e975b6c50
Noel Riopel <nriopel@sfu.ca>
2022-07-13 20:07:25 -0700

    Implement mock sign up button click handler

--- app/src/main/java/com/example/hubspot/LoginActivity.kt
+++ app/src/main/java/com/example/hubspot/LoginActivity.kt
@@ -3,7 +3,10 @@ package com.example.hubspot
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hubspot.services.Auth
 
 
 class LoginActivity : AppCompatActivity() {
@@ -18,5 +21,24 @@ class LoginActivity : AppCompatActivity() {
         finish()
     }
 
-    fun onSignUpButtonClick(view: View) {}
+    fun onSignUpButtonClick(view: View) {
+        val emailEditText = findViewById<EditText>(R.id.login_edittext_email)
+        val passwordEditText = findViewById<EditText>(R.id.login_edittext_password)
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        val result = Auth.sendAccountActivationEmail(email, password)
+
+        if (result == Auth.SendAccountActivationEmailResult.SUCCESS) {
+            Toast.makeText(this, "Check your email to activate your account!", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            Toast.makeText(
+                this,
+                "Failed to send account creation email. Error: $result",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+    }
 }
