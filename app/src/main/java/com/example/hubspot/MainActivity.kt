49083e0320cd0513fc5fbf58ff8b5186a80268bf
catnip1032 <catherine_riopel@hotmail.com>
2022-07-14 19:27:40 -0700

    Change location of security fragment for import

--- app/src/main/java/com/example/hubspot/MainActivity.kt
+++ app/src/main/java/com/example/hubspot/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.hubspot.ratings.RatingsFragment
 import com.example.hubspot.schedule.ScheduleFragment
-import com.example.hubspot.security.SecurityFragment
+import com.example.hubspot.security.ui.SecurityFragment
 import com.example.hubspot.studybuddy.StudyBuddyFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
