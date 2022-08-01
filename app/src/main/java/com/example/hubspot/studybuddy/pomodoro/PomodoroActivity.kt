b39f4a41d46ff20e07a802973e3ba10327926ef4
rshimaro <rshimaro@sfu.ca>
2022-07-31 19:57:35 -0700

    Fixed bugs in pomodoro timer

--- app/src/main/java/com/example/hubspot/studybuddy/pomodoro/PomodoroActivity.kt
+++ app/src/main/java/com/example/hubspot/studybuddy/pomodoro/PomodoroActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.hubspot.R
 import com.example.hubspot.utils.Dialog
 import me.zhanghai.android.materialprogressbar.MaterialProgressBar
+import java.util.*
 
 class PomodoroActivity: AppCompatActivity()  {
     companion object{
@@ -15,7 +16,6 @@ class PomodoroActivity: AppCompatActivity()  {
         const val BREAK_SESSION = "break"
         const val TAG:String = "tag"
     }
-
     private lateinit var sessionTypeTextView: TextView
     private lateinit var textView: TextView
     private lateinit var startButton: Button
@@ -25,12 +25,11 @@ class PomodoroActivity: AppCompatActivity()  {
     private lateinit var progressBar: MaterialProgressBar
     private var initStudyLengthInSeconds = 0L
     private var initBreakLengthInSeconds = 0L
-    private var secondsUntilFinished = 0L
-    private lateinit var timerState: TimerState
+    private var timeLeftInMillis = 0L
+    private var endTime = 0L
     private lateinit var sessionType: String
-    private enum class TimerState{
-        Paused, Running
-    }
+    private var timerIsRunning = false
+    private var timerIsPaused = false
     private val dialog = Dialog()
     private val bundle = Bundle()
 
@@ -43,63 +42,101 @@ class PomodoroActivity: AppCompatActivity()  {
         pauseButton = this.findViewById(R.id.timerPauseButton)
         stopButton = this.findViewById(R.id.timerStopButton)
         progressBar = this.findViewById(R.id.materialProgressBar)
-        initStudyLengthInSeconds = intent.getLongExtra("studyLength", 0L)
-        initBreakLengthInSeconds = intent.getLongExtra("breakLength", 0L)
+        initStudyLengthInSeconds = intent.getLongExtra("studyLength", 0L) * 1000
+        initBreakLengthInSeconds = intent.getLongExtra("breakLength", 0L) * 1000
         sessionType = intent.getStringExtra("sessionType")!!
         if (sessionType == STUDY_SESSION) {
-            secondsUntilFinished = initStudyLengthInSeconds
+            timeLeftInMillis = initStudyLengthInSeconds
             progressBar.max = initStudyLengthInSeconds.toInt()
             sessionTypeTextView.text = getString(R.string.study_session_title)
         } else {
-            secondsUntilFinished = initBreakLengthInSeconds
+            timeLeftInMillis = initBreakLengthInSeconds
             progressBar.max = initBreakLengthInSeconds.toInt()
             sessionTypeTextView.text = getString(R.string.break_session_title)
         }
 
-        startButton.setOnClickListener{onTimerStart()}
-        pauseButton.setOnClickListener{onTimerPause()}
+        startButton.setOnClickListener{startTimer()}
+        pauseButton.setOnClickListener{pauseTimer()}
         stopButton.setOnClickListener{onTimerStop()}
 
-        onTimerStart()
-        timerState = TimerState.Running
+        if (savedInstanceState == null || !savedInstanceState.getBoolean("timerIsRunning")) {
+            startTimer()
+        }
     }
 
-    private fun onTimerStart(){
+    private fun startTimer() {
         startButton.isEnabled = false
-        timer = object : CountDownTimer( secondsUntilFinished * 1000, 50) {
+        pauseButton.isEnabled = true
+        endTime = System.currentTimeMillis() + timeLeftInMillis
+        timer = object : CountDownTimer(timeLeftInMillis, 10) {
             override fun onTick(millisUntilFinished: Long) {
-                secondsUntilFinished = millisUntilFinished / 1000
-                val minutesUntilFinished = (secondsUntilFinished) / 60
-                val secondsInMinuteUntilFinished = secondsUntilFinished - minutesUntilFinished * 60
-                val secondsStr = if (secondsInMinuteUntilFinished.toString().length == 2) secondsInMinuteUntilFinished else "0$secondsInMinuteUntilFinished"
-                textView.text = getString(R.string.timer, "$minutesUntilFinished:$secondsStr")
-                progressBar.progress = if (sessionType == STUDY_SESSION) (initStudyLengthInSeconds - secondsUntilFinished).toInt() else (initBreakLengthInSeconds - secondsUntilFinished).toInt()
+                timeLeftInMillis = millisUntilFinished
+                updateCountDownText()
             }
 
             override fun onFinish() {
-                bundle.putLong("studyLength", initStudyLengthInSeconds)
-                bundle.putLong("breakLength", initBreakLengthInSeconds)
-                if(sessionType == STUDY_SESSION) {
-                    bundle.putInt(Dialog.DIALOG_KEY, Dialog.POMODORO_START_BREAK_DIALOG)
-                } else if (sessionType == BREAK_SESSION) {
-                    bundle.putInt(Dialog.DIALOG_KEY, Dialog.POMODORO_START_STUDY_DIALOG)
+                timerIsRunning = false
+                if (!supportFragmentManager.isDestroyed) {
+                    bundle.putLong("studyLength", initStudyLengthInSeconds)
+                    bundle.putLong("breakLength", initBreakLengthInSeconds)
+                    if (sessionType == STUDY_SESSION) {
+                        bundle.putInt(Dialog.DIALOG_KEY, Dialog.POMODORO_START_BREAK_DIALOG)
+                    } else if (sessionType == BREAK_SESSION) {
+                        bundle.putInt(Dialog.DIALOG_KEY, Dialog.POMODORO_START_STUDY_DIALOG)
+                    }
+                    dialog.arguments = bundle
+                    dialog.show(supportFragmentManager, TAG)
                 }
-                dialog.arguments = bundle
-                dialog.show(supportFragmentManager, TAG)
             }
         }.start()
+        timerIsRunning = true
+        timerIsPaused = false
     }
 
-    private fun onTimerPause() {
-        timer.cancel()
-        timerState = TimerState.Paused
+    private fun pauseTimer() {
+        pauseButton.isEnabled = false
         startButton.isEnabled = true
+        endTime = System.currentTimeMillis() + timeLeftInMillis
+        timer.cancel()
+        updateCountDownText()
+        timerIsPaused = true
     }
 
-    private fun onTimerStop(){
-        if(timerState == TimerState.Running) {
-            timer.cancel()
-        }
+    private fun onTimerStop() {
+        timer.cancel()
         this.finish()
+    }
+
+    private fun updateCountDownText() {
+        val minutes = (timeLeftInMillis / 1000).toInt() / 60
+        val seconds = (timeLeftInMillis / 1000).toInt() % 60
+        val timeLeftFormatted: String =
+            java.lang.String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+        textView.text = timeLeftFormatted
+        progressBar.progress = if (sessionType == STUDY_SESSION) (initStudyLengthInSeconds - timeLeftInMillis).toInt() else (initBreakLengthInSeconds - timeLeftInMillis).toInt()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("timeLeftInMillis", timeLeftInMillis)
+        outState.putBoolean("timerIsRunning", timerIsRunning)
+        outState.putBoolean("timerIsPaused", timerIsPaused)
+        outState.putLong("endTime", endTime)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        timeLeftInMillis = savedInstanceState.getLong("timeLeftInMillis")
+        timerIsRunning = savedInstanceState.getBoolean("timerIsRunning")
+        timerIsPaused = savedInstanceState.getBoolean("timerIsPaused")
+        updateCountDownText()
+        if (timerIsRunning && !timerIsPaused) {
+            endTime = savedInstanceState.getLong("endTime")
+            timeLeftInMillis = endTime - System.currentTimeMillis()
+            startTimer()
+        }
+        if(timerIsPaused) {
+            pauseButton.isEnabled = false
+        }
     }
 }
