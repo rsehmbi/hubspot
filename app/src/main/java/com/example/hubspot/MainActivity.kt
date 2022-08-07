a69ba83d3637a58f0dcbc8f4c1eaf50ae3887089
NoelRiopel <nriopel@sfu.ca>
2022-08-07 14:42:59 -0700

    Add initial version of side navigation menu

--- app/src/main/java/com/example/hubspot/MainActivity.kt
+++ app/src/main/java/com/example/hubspot/MainActivity.kt
@@ -3,23 +3,26 @@ package com.example.hubspot
 import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.hubspot.auth.Auth
 import com.example.hubspot.databinding.ActivityMainBinding
 import com.example.hubspot.login.LoginActivity
 import com.example.hubspot.profile.ProfileActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.hubspot.schedule.ScheduleFragment
+import com.example.hubspot.security.ui.SecurityFragment
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -33,22 +36,75 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.bottomNavigationView
-        val navController = findNavController(R.id.flFragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_schedule, R.id.navigation_ratings, R.id.navigation_studdybuddy,
-                R.id.navigation_security, R.id.navigation_friends
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        replaceActionBarWithToolBar()
 
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        // Setup drawer view
+        val drawer = binding.nvView
+        setupDrawerContent(drawer);
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.options_menu, menu)
-        return true
+    private fun replaceActionBarWithToolBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            binding.drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            selectDrawerItem(menuItem)
+            true
+        }
+
+        // Use schedule fragment as default fragment
+        setDefaultFragment(ScheduleFragment::class.java)
+    }
+
+    private fun <T> setDefaultFragment(fragmentClass:  Class<T>) {
+        var fragment: Fragment? = null
+        try {
+            fragment = fragmentClass.newInstance() as Fragment
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val fragmentManager: FragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment!!).commit()
+    }
+
+    fun selectDrawerItem(menuItem: MenuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        var fragment: Fragment? = null
+        val fragmentClass: Class<*>
+        fragmentClass = when (menuItem.itemId) {
+            R.id.side_drawer_menu_item_schedule -> ScheduleFragment::class.java
+            R.id.side_drawer_menu_item_security -> SecurityFragment::class.java
+            else -> ScheduleFragment::class.java
+        }
+        try {
+            fragment = fragmentClass.newInstance() as Fragment
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        val fragmentManager: FragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment!!).commit()
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.isChecked = true
+        // Set action bar title
+        title = menuItem.title
+        // Close the navigation drawer
+        binding.drawerLayout.closeDrawers()
     }
 
     private fun sendVolumeBroadcast(action: Int, keyCode: Int) {
