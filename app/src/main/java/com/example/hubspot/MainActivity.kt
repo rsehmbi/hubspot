610851810270b2aed381007d39857e2ae94b3819
rshimaro <rshimaro@sfu.ca>
2022-08-07 20:56:46 -0700

    Check the internet connection before and while using the app

--- app/src/main/java/com/example/hubspot/MainActivity.kt
+++ app/src/main/java/com/example/hubspot/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.hubspot
 
 import android.content.Intent
 import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -34,6 +39,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if(!NetworkUtil.isOnline(this)) {
+            Toast.makeText(this, "Please connect to the Internet", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
         // Check if user is currently logged in, go to login screen if not
         if (Auth.getCurrentUser() == null) {
             val intent = Intent(this, LoginActivity::class.java)
@@ -62,6 +72,24 @@ class MainActivity : AppCompatActivity() {
 
         // Tie DrawerLayout events to the ActionBarToggle
         binding.drawerLayout.addDrawerListener(drawerToggle)
+
+        // Register a listener for the internet connection
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+        val connectivityManager = getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+    }
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Toast.makeText(this@MainActivity, "Please connect to the Internet", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
     private fun setupHamburgerButton() {
--- app/src/main/java/com/example/hubspot/services/FireBaseDateBaseService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hubspot.services
-
-object FireBaseDateBaseService {
-
-
-
-
-
-}
