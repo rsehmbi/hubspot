50d74c2fc708bb924f5dc3308eee517cf6d657ed
catnip1032 <catherine_riopel@hotmail.com>
2022-08-08 19:45:48 -0700

    Create Speech class for use with firebase realtime database

---
+++ app/src/main/java/com/example/hubspot/security/models/Speech.kt
@@ -0,0 +1,7 @@
+package com.example.hubspot.security.models
+
+/**
+ * Speech class is used for uploading and reading speech data to firebase realtime database.
+ */
+class Speech(var time: Long = 0L, var speech: String = "") {
+}
